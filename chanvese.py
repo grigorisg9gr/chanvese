b8eb7a87f6e28b63f98682e626907e84dff0b571
Grigoris <grigoris.chrysos@gmail.com>
2015-07-29 17:39:20 +0100

    Fix comments and indentation with comments.

--- chanvese.py
+++ chanvese.py
@@ -32,87 +32,81 @@ import matplotlib.pyplot as plt
 
 eps = np.finfo(np.float).eps
     
-def chanvese(I,init_mask,max_its=200,alpha=0.2,thresh=0,color='r',display=False):
+def chanvese(I, init_mask, max_its=200, alpha=0.2, thresh=0, color='r', display=False):
 
     I = I.astype('float')
-    
-    #-- Create a signed distance map (SDF) from mask
+    #  Create a signed distance map (SDF) from mask
     phi = mask2phi(init_mask)
 
     if display:
         plt.ion()
         showCurveAndPhi(I, phi, color)
-        plt.savefig('levelset_start.png',bbox_inches='tight')
+        plt.savefig('levelset_start.png', bbox_inches='tight')
     
-    #--main loop
+    # main loop
     its = 0
     stop = False
     prev_mask = init_mask
     c = 0
       
     while (its < max_its and not stop):
-
         # get the curve's narrow band
-        idx = np.flatnonzero( np.logical_and( phi <= 1.2, phi >= -1.2) )
+        idx = np.flatnonzero(np.logical_and(phi <= 1.2, phi >= -1.2))
         
         if len(idx) > 0:
-            #-- intermediate output
+            #  intermediate output
             if display:
-                if np.mod(its,50) == 0:            
+                if np.mod(its, 50) == 0:
                     print 'iteration:', its
                     showCurveAndPhi(I, phi, color)
 
-            else:
-                if np.mod(its,10) == 0:
-                    print 'iteration:', its
-
-            #-- find interior and exterior mean
-            upts = np.flatnonzero(phi<=0)                 # interior points
-            vpts = np.flatnonzero(phi>0)                  # exterior points
-            u = np.sum(I.flat[upts])/(len(upts)+eps) # interior mean
-            v = np.sum(I.flat[vpts])/(len(vpts)+eps) # exterior mean
+            #  find interior and exterior mean
+            upts = np.flatnonzero(phi <= 0)                 # interior points
+            vpts = np.flatnonzero(phi > 0)                  # exterior points
+            u = np.sum(I.flat[upts])/(len(upts)+eps)  # interior mean
+            v = np.sum(I.flat[vpts])/(len(vpts)+eps)  # exterior mean
 
             F = (I.flat[idx]-u)**2-(I.flat[idx]-v)**2    # force from image information
             curvature = get_curvature(phi,idx)  # force from curvature penalty
 
             dphidt = F /np.max(np.abs(F)) + alpha*curvature # gradient descent to minimize energy
 
-            #-- maintain the CFL condition
+            #  maintain the CFL condition
             dt = 0.45/(np.max(np.abs(dphidt))+eps)
 
-            #-- evolve the curve
+            #  evolve the curve
             phi.flat[idx] += dt*dphidt
 
-            #-- Keep SDF smooth
+            #  Keep SDF smooth
             phi = sussman(phi, 0.5)
             
             new_mask = phi<=0
             c = convergence(prev_mask,new_mask,thresh,c)
 
             if c <= 5:
-                its = its + 1
+                its += 1
                 prev_mask = new_mask
-            else: stop = True
-
+            else:
+                stop = True
         else:
             break
 
-    #-- final output
+    #  final output
     if display:
         showCurveAndPhi(I, phi, color)
         plt.savefig('levelset_end.png',bbox_inches='tight')         
 
-    #-- make mask from SDF
-    seg = phi<=0 #-- Get mask from levelset
+    # make mask from SDF
+    seg = phi <= 0  # Get mask from levelset
   
     return seg,phi,its
 
 
-#---------------------------------------------------------------------
-#---------------------------------------------------------------------
-#-- AUXILIARY FUNCTIONS ----------------------------------------------
-#---------------------------------------------------------------------
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# -- AUXILIARY FUNCTIONS ----------------------------------------------
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 
 def bwdist(a):
     """ 
@@ -124,7 +118,7 @@ def bwdist(a):
 
 import time
 
-#-- Displays the image with curve superimposed
+# Displays the image with curve superimposed
 def showCurveAndPhi(I, phi, color):
     myplot = plt.subplot(121)
     myplot.cla()
@@ -152,26 +146,27 @@ def im2double(a):
     a /= a.max()
     return a
     
-#-- converts a mask to a SDF
+# converts a mask to a SDF
 def mask2phi(init_a):
-    phi = bwdist(init_a)-bwdist(1-init_a)+im2double(init_a) -0.5
+    # init_a should be float with values in [0, 1].
+    phi = bwdist(init_a)-bwdist(1-init_a)+init_a -0.5
     return phi
   
-#-- compute curvature along SDF
+# compute curvature along SDF
 def get_curvature(phi,idx):
     dimy, dimx = phi.shape       
     yx = np.array([np.unravel_index(i, phi.shape)for i in idx])  # get subscripts
     y = yx[:,0]
     x = yx[:,1]
 
-    #-- get subscripts of neighbors
+    # get subscripts of neighbors
     ym1 = y-1; xm1 = x-1; yp1 = y+1; xp1 = x+1;
 
-    #-- bounds checking  
+    # bounds checking
     ym1[ym1<0] = 0; xm1[xm1<0] = 0;              
     yp1[yp1>=dimy]=dimy - 1; xp1[xp1>=dimx] = dimx - 1;    
 
-    #-- get indexes for 8 neighbors
+    # get indexes for 8 neighbors
     idup = np.ravel_multi_index( (yp1,x),phi.shape)
     iddn = np.ravel_multi_index( (ym1,x),phi.shape)
     idlt = np.ravel_multi_index( (y,xm1),phi.shape)
@@ -181,7 +176,7 @@ def get_curvature(phi,idx):
     iddl = np.ravel_multi_index( (ym1,xm1),phi.shape)
     iddr = np.ravel_multi_index( (ym1,xp1),phi.shape)
     
-    #-- get central derivatives of SDF at x,y
+    # get central derivatives of SDF at x,y
     phi_x  = -phi.flat[idlt]+phi.flat[idrt]
     phi_y  = -phi.flat[iddn]+phi.flat[idup]
     phi_xx = phi.flat[idlt]-2*phi.flat[idx]+phi.flat[idrt]
@@ -191,20 +186,20 @@ def get_curvature(phi,idx):
     phi_x2 = phi_x**2
     phi_y2 = phi_y**2
     
-    #-- compute curvature (Kappa)
+    # compute curvature (Kappa)
     curvature = ( ((phi_x2*phi_yy + phi_y2*phi_xx - 2*phi_x*phi_y*phi_xy)
                    / (phi_x2 + phi_y2 +eps)**(3/2))
                   *(phi_x2 + phi_y2)**(1/2))
 
     return curvature
 
-#-- level set re-initialization by the sussman method
+# level set re-initialization by the sussman method
 def sussman(D, dt):
     # forward/backward differences
-    a = D - shiftR(D) # backward
-    b = shiftL(D) - D # forward
-    c = D - shiftD(D) # backward
-    d = shiftU(D) - D # forward
+    a = D - shiftR(D)  # backward
+    b = shiftL(D) - D  # forward
+    c = D - shiftD(D)  # backward
+    d = shiftU(D) - D  # forward
 
     a_p = a.copy();  a_n = a.copy(); # a+ and a-
     b_p = b.copy();  b_n = b.copy();
@@ -224,22 +219,22 @@ def sussman(D, dt):
     D_neg_ind = np.flatnonzero(D < 0)
     D_pos_ind = np.flatnonzero(D > 0)
     
-    dD.flat[D_pos_ind] = np.sqrt( np.max( np.concatenate( ([a_p.flat[D_pos_ind]**2],
-                                                           [b_n.flat[D_pos_ind]**2]) ),
-                                          axis=0
+    dD.flat[D_pos_ind] = np.sqrt( np.max(np.concatenate(([a_p.flat[D_pos_ind]**2],
+                                                          [b_n.flat[D_pos_ind]**2])),
+                                         axis=0
                                           )
-                                  + np.max( np.concatenate( ([c_p.flat[D_pos_ind]**2],
-                                                             [d_n.flat[D_pos_ind]**2])),
+                                  + np.max( np.concatenate(([c_p.flat[D_pos_ind]**2],
+                                                            [d_n.flat[D_pos_ind]**2])),
                                             axis=0
                                             )
                                   ) - 1
-    dD.flat[D_neg_ind] = np.sqrt( np.max( np.concatenate( ([a_n.flat[D_neg_ind]**2],
-                                                           [b_p.flat[D_neg_ind]**2])),
-                                          axis=0
+    dD.flat[D_neg_ind] = np.sqrt( np.max(np.concatenate(([a_n.flat[D_neg_ind]**2],
+                                                          [b_p.flat[D_neg_ind]**2])),
+                                         axis=0
                                           )
-                                  + np.max( np.concatenate( ([c_n.flat[D_neg_ind]**2],
-                                                             [d_p.flat[D_neg_ind]**2]) ),
-                                            axis=0
+                                  + np.max(np.concatenate(([c_n.flat[D_neg_ind]**2],
+                                                            [d_p.flat[D_neg_ind]**2]) ),
+                                           axis=0
                                             )
                                   ) - 1
 
@@ -247,7 +242,7 @@ def sussman(D, dt):
     
     return D
   
-#-- whole matrix derivatives
+# whole matrix derivatives
 def shiftD(M):
     return shiftR(M.transpose()).transpose()
 
@@ -266,7 +261,7 @@ def sussman_sign(D):
     return D / np.sqrt(D**2 + 1)    
 
 # Convergence Test
-def convergence(p_mask,n_mask,thresh,c):
+def convergence(p_mask, n_mask, thresh, c):
     diff = p_mask - n_mask
     n_diff = np.sum(np.abs(diff))
     if n_diff < thresh:
